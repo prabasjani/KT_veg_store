5702a933e0e60f4afa6c7875ef839538e337393f
Prabanjan <itsprabanjan@gmail.com>
2025-03-11 11:23:36 +0530

    Initial commit Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl text-red-600 font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
